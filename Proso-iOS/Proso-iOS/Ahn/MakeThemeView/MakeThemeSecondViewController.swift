af05099ed4c23c3cab508592008846930e07ad67
Jason <synoti21@naver.com>
2022-08-08 03:01:18 +0900

    Theme Make Second Page

--- Proso-iOS/Proso-iOS/Ahn/MakeThemeView/MakeThemeSecondViewController.swift
+++ Proso-iOS/Proso-iOS/Ahn/MakeThemeView/MakeThemeSecondViewController.swift
@@ -9,24 +9,22 @@ import Foundation
 import UIKit
 import SnapKit
 
-class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMapViewDelegate{
+class MakeThemeSecondViewController: UIViewController, UITextFieldDelegate, MTMapViewDelegate, UITextViewDelegate{
         
     var mapview: MTMapView!
     
 // MARK: - 구성 요소 추가
-    var textCount = 0 ///입력 칸 글자 수 카운트
-    
     let progressBar: UIProgressView = { ///진행 뷰
         let progressView = UIProgressView()
         progressView.progressViewStyle = .bar
         progressView.progressTintColor = #colorLiteral(red: 1, green: 0.4265864491, blue: 0.4015736282, alpha: 1)
-        progressView.progress = 0.2
+        progressView.progress = 0.4
         return progressView
     }()
     
     let themeQustion: UILabel = { ///테마 만들기 질문
         let label = UILabel()
-        label.text = "테마 이름을 입력해주세요."
+        label.text = "테마 소개글을 입력해주세요."
         label.font = UIFont.boldSystemFont(ofSize: 18.0)
         label.adjustsFontSizeToFitWidth = true
         label.minimumScaleFactor = 0.2
@@ -34,32 +32,18 @@ class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMap
         return label
     }()
     
-    let inputField: UITextField = { ///사용자 입력 칸, 디자인이 밑줄 형태이므로 frame 커스텀화
-        let textField = UITextField()
+    let inputField: UITextView = { ///textfield는 여러 줄 처리가 불가능하므로 textView로 대체
+        let textView = UITextView()
         
-        textField.placeholder = "내용 입력"
-        textField.font = UIFont.boldSystemFont(ofSize: 14.0)
-        textField.adjustsFontSizeToFitWidth = true
+        textView.isEditable = true
+        textView.layer.borderColor = #colorLiteral(red: 0.8861967921, green: 0.8861967921, blue: 0.8861967921, alpha: 1)
+        textView.font = UIFont.systemFont(ofSize: 14)
+        textView.layer.borderWidth = CGFloat(1.0)
+        textView.layer.cornerRadius = CGFloat(8.0)
+        textView.textContainerInset = UIEdgeInsets(top: 16, left: 12, bottom: 16, right: 12);
 
-        return textField
-    }()
-    
-    let inputFieldUnderLine: UIView = {
-        let textFieldLine = UIView()
-        textFieldLine.backgroundColor = #colorLiteral(red: 0.8861967921, green: 0.8861967921, blue: 0.8861967921, alpha: 1)
         
-        return textFieldLine
-    }()
-    
-    let textCountLabel: UILabel = { ///입력 글자 수 표시 (입력칸 아래에)
-        let label = UILabel()
-        label.text = "0/0"
-        label.font = UIFont.systemFont(ofSize: 12.0)
-        label.adjustsFontSizeToFitWidth = true
-        label.minimumScaleFactor = 0.2
-        label.textColor = #colorLiteral(red: 0.4756370187, green: 0.4756369591, blue: 0.4756369591, alpha: 1)
-        
-        return label
+        return textView
     }()
     
     let nextButton: UIButton = { ///다음 버튼
@@ -103,16 +87,9 @@ class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMap
         
         inputField.snp.makeConstraints{
             $0.top.equalTo(themeQustion.snp.bottom).offset(24)
-            $0.height.equalTo(20)
+            $0.height.equalTo(108)
             $0.leading.equalToSuperview().offset(20)
             $0.trailing.equalToSuperview().offset(-20)
-        }
-        
-        inputFieldUnderLine.snp.makeConstraints{
-            $0.height.equalTo(1)
-            $0.leading.equalToSuperview().offset(20)
-            $0.trailing.equalToSuperview().offset(-20)
-            $0.top.equalTo(inputField.snp.bottom).offset(8)
         }
         
         nextButton.snp.makeConstraints{
@@ -122,22 +99,19 @@ class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMap
             $0.height.equalTo(56)
         }
         
-        textCountLabel.snp.makeConstraints{
-            $0.top.equalTo(inputFieldUnderLine.snp.bottom).offset(10)
-            $0.trailing.equalToSuperview().offset(-20)
-        }
+        inputField.textColor = .lightGray
+        inputField.text = "내용 입력"
+    
         
-        inputField.addTarget(self, action: #selector(self.textFieldDidChange(textField:)), for: UIControl.Event.editingChanged) ///글자 수 카운트를 위한 설정
-        
-        nextButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside) ///다음 버튼 활성화/비활성화를 위한 설정
+        nextButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
     }
     
 // MARK: - 컴포넌트 추가 및 상단 네비게이션 바 설정
     private func addView(){
-        [progressBar,themeQustion,inputField,inputFieldUnderLine,textCountLabel,nextButton].forEach({
+        [progressBar,themeQustion,inputField,nextButton].forEach({
             self.view.addSubview($0)
         })
-        inputField.delegate = self ///입력 칸 글자 수 카운트를 위한 delegate 설정
+        inputField.delegate = self ///입력 칸 글자 수 카운트
     }
     
     private func setUpNavigationBar() {
@@ -154,20 +128,11 @@ class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMap
     
     @objc func dismissSelf(){
         dismiss(animated: true, completion: nil)
-    } ///뒤로 가기 버튼
-    
-// MARK: - 입력 칸 글자 수 제한
-    internal func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        let maxLength = 30
-        let currentString = (textField.text ?? "") as NSString
-        let newString = currentString.replacingCharacters(in: range, with: string)
-
-        return newString.count <= maxLength
     }
-// MARK: - 실시간 글자 수 계산
-    @objc func textFieldDidChange(textField : UITextField){
-        textCount = inputField.text!.count
-        textCountLabel.text = "\(String(describing: textCount))/30"
+    
+// MARK: - 버튼 활성화/비활성화
+    func textViewDidChange(_ textView: UITextView) {
+        let textCount = inputField.text!.count
         if(textCount > 0){
             nextButton.backgroundColor = #colorLiteral(red: 1, green: 0.4265864491, blue: 0.4015736282, alpha: 1)
             nextButton.isEnabled = true
@@ -177,14 +142,37 @@ class MakeThemeFirstViewController: UIViewController, UITextFieldDelegate, MTMap
         }
     }
     
+// MARK: - TextView Placeholder 설정
+    func textViewDidBeginEditing (_ textView: UITextView) {
+        if inputField.textColor == .lightGray && inputField.isFirstResponder {
+            inputField.text = nil
+            inputField.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing (_ textView: UITextView) {
+        if inputField.text.isEmpty || inputField.text == "" {
+            inputField.textColor = .lightGray
+            inputField.text = "내용 입력"
+        }
+    }
+    
 // MARK: - 다음으로 이동
     @objc private func didTapButton(){
         
-        ///TextField의 Text를 불러와 서버에 넣는 함수. 또는 다음 뷰 컨트롤러에 데이터를 전달
-        
-        let rootVC = MakeThemeSecondViewController()
+        ///TextView의 Text를 불러와 서버에 넣는 코드. 또는 다음 뷰 컨트롤러에 정보 전달
+
+        let rootVC = MakeThemeThirdViewController()
         let navVC = UINavigationController(rootViewController: rootVC)
         navVC.modalPresentationStyle = .fullScreen
         present(navVC, animated: false)
+    }
+}
+
+extension UITextField {
+    func setLeftPaddingPoints(){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
     }
 }
---
+++ Proso-iOS/Proso-iOS/Ahn/MakeThemeView/MakeThemeThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MakeThemeThirdViewController.swift
+//  Proso-iOS
+//
+//  Created by 안지완 on 2022/08/08.
+//
+import Foundation
+import UIKit
+import SnapKit
+
+class MakeThemeThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
